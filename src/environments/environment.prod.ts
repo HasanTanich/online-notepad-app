3881d83c4d3f2ad01789f4f9427b8503edd125c0
Hasan Tanich <hasantanich@gmail.com>
2023-02-25 20:42:34 +0100

    new database

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyBoAm969Jg_4V5B6SgF9a6RCylT0KcldpI",
-    authDomain: "online-digital-notepad.firebaseapp.com",
-    projectId: "online-digital-notepad",
-    storageBucket: "online-digital-notepad.appspot.com",
-    messagingSenderId: "633886395662",
-    appId: "1:633886395662:web:262cac4614e490266b1b5f",
-    measurementId: "G-JDK4V7KQJD"
-  }
+  firebaseConfig : {
+    apiKey: "AIzaSyBONMBj11c9ZLDo2MEIfi8FRvl50lFr46g",
+    authDomain: "online-notepad-application.firebaseapp.com",
+    projectId: "online-notepad-application",
+    storageBucket: "online-notepad-application.appspot.com",
+    messagingSenderId: "956850785716",
+    appId: "1:956850785716:web:0fed7b5d733846c8cbf6e1",
+    measurementId: "G-E4CEM7GJ1Z"
+  },
 };
